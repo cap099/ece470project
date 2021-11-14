291d4e1ba791188bba1c7464ee824dbe92afe96f
caleb <calebpatton09@gmail.com>
2021-11-13 21:45:06 -0600

    project update final complete

---
+++ catkin_ws/src/ece470/controllers/ure_can_grasper/ur3_controller.py
@@ -0,0 +1,17 @@
+from controller import Robot, Motor, distance_sensor
+
+
+
+
+
+
+f1j1 = robot.getDevice('finger_1_joint_1')
+f2j1 = robot.getDevice('finger_2_joint_1')
+middle = robot.getDevice('finger_middle_joint_1')
+
+
+
+
+
+while(True):
+
